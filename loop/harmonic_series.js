72d0cf382fd49cee5434db489c62bd11b167b8f9
Ria <sumiaria70@gmail.com>
2024-02-13 20:36:01 +0600

    solved harmonic series

---
+++ loop/harmonic_series.js
@@ -0,0 +1,23 @@
+// Write a program in JavaScript (NodeJS) to display the n terms of a harmonic series and their sum.
+// 1 + 1/2 + 1/3 + 1/4 + 1/5 ... 1/n terms
+// Test Data :
+// Input the number of terms : 5
+// Expected Output :
+// 1/1 + 1/2 + 1/3 + 1/4 + 1/5 = 2.283334
+
+let n = 5
+let i = 1
+let res = 0
+
+while (i <= n){
+    sum = 1/i
+    res= res + sum
+    if(i != n){
+       process.stdout.write(`1/${i} + `);
+    }
+    else{
+        process.stdout.write(`1/${i} `);
+    }
+    i++
+}
+process.stdout.write(`= ${res}`);
