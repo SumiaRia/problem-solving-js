5e2296519cc5d268424e940afccf1478929755e3
Ria <sumiaria70@gmail.com>
2024-02-28 13:43:46 +0600

    solved nested loop problems

---
+++ loop/pyramid_3.js
@@ -0,0 +1,43 @@
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+let number = 5
+for(let j= number; j > 0; j--){
+    for(let i = 0; i < number; i++){
+        if(i<(number-j)){
+            process.stdout.write(` `)
+        }
+        else{
+
+            process.stdout.write(`*`)
+        }
+    }
+    for(let i = 1; i < j; i++){
+        process.stdout.write("*")
+    }
+    process.stdout.write("\n")
+
+}
+
+for(let j = 0; j < number; j++){
+    for(let i = 1; i <= number; i++){
+        if(i < (number - j)){
+            process.stdout.write(` `)
+        }
+        else{
+            process.stdout.write(`*`)
+        }
+    }
+    for(let i = 1; i <= j; i++){
+        process.stdout.write(`*`)
+    }
+    process.stdout.write(`\n`)
+}
--- loop/star_triangle.js
+++ loop/star_triangle.js
@@ -6,12 +6,12 @@
 // *****
 // let number = 1
 let number = 5
-// for(let count = 1; count <= number; count++){
-//     for (let j = 1; j <= count; j++){
-//         process.stdout.write(`*`)
-//     }
-//     process.stdout.write("\n")
-// }
+for(let count = 1; count <= number; count++){
+    for (let j = 1; j <= count; j++){
+        process.stdout.write(`*`)
+    }
+    process.stdout.write("\n")
+}
 
 
 
